a0d9d357c3e579ccdc964d89215ad78146cdc2a8
deliahellander <70303141+deliahellander@users.noreply.github.com>
2020-12-05 10:34:54 -0500

    modified sql format

--- Load/database.sql
+++ Load/database.sql
@@ -1,5 +1,6 @@
  
  -- Showing some steps in the Load Process
+
         -- test
         SELECT * FROM "Cuisine_Type";
 
@@ -13,13 +14,15 @@
         ALTER TABLE "Google_Maps_Rating" ALTER COLUMN google_maps_rating TYPE varchar;
         SELECT * FROM "Google_Maps_Rating";
         ALTER TABLE "Yelp_Rating" ALTER COLUMN yelp_rating DROP NOT NULL;
+
 -- Query Example
--- Displaying restaurants reviews from places that has reviews on all three websites(Yelp, Google Maps, TripAdvisor)
-SELECT "Restaurant_Info".restaurant_id, "Restaurant_Info".restaurant_name,"Restaurant_Info".restaurant_website,
-       "Yelp_Rating".yelp_rating, "Google_Maps_Rating".google_maps_rating
-FROM "Restaurant_Info"
-INNER JOIN "Yelp_Rating" ON "Restaurant_Info".restaurant_id = "Yelp_Rating".restaurant_id
-INNER JOIN "Google_Maps_Rating" ON "Google_Maps_Rating".restaurant_id = "Yelp_Rating".restaurant_id
-INNER JOIN "Trip_Advisor_Rating" ON "Trip_Advisor_Rating".restaurant_id = "Google_Maps_Rating".restaurant_id;
+
+        -- Displaying restaurants reviews from places that has reviews on all three websites(Yelp, Google Maps, TripAdvisor)
+        SELECT "Restaurant_Info".restaurant_id, "Restaurant_Info".restaurant_name,"Restaurant_Info".restaurant_website,
+        "Yelp_Rating".yelp_rating, "Google_Maps_Rating".google_maps_rating
+        FROM "Restaurant_Info"
+        INNER JOIN "Yelp_Rating" ON "Restaurant_Info".restaurant_id = "Yelp_Rating".restaurant_id
+        INNER JOIN "Google_Maps_Rating" ON "Google_Maps_Rating".restaurant_id = "Yelp_Rating".restaurant_id
+        INNER JOIN "Trip_Advisor_Rating" ON "Trip_Advisor_Rating".restaurant_id = "Google_Maps_Rating".restaurant_id;
 
 
